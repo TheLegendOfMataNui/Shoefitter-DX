9a82841054dce5b4e2149f6dd7ced25d66ba7101
Ben Garcia <pokeboygarcia@gmail.com>
2019-02-02 01:27:02 -0800

    Add OSI reading and graph and testing browser.

--- ShoefitterDX/Program.cs
+++ ShoefitterDX/Program.cs
@@ -20,9 +20,23 @@ namespace ShoefitterDX
         {
             Config = new SAGESharp.INIConfig(INIFilename);
 
+            SAGESharp.OSI.OSIFile osi;
+
+            using (System.IO.FileStream stream = new System.IO.FileStream("D:\\Program Files\\LEGO Bionicle\\Data\\Base.osi", System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.Read))
+            using (System.IO.BinaryReader reader = new System.IO.BinaryReader(stream))
+            {
+                osi = new SAGESharp.OSI.OSIFile(reader);
+
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Window());
+            //Application.Run(new Window());
+
+            OSIBrowser browser = new OSIBrowser();
+            browser.LoadOSI(osi);
+            Application.Run(browser);
+
             Config.Write(INIFilename);
         }
     }
