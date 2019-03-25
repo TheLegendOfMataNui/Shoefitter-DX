aa99d6a4c99438498d9aa62879ad9a693f275999
Ben Garcia <pokeboygarcia@gmail.com>
2019-03-24 23:04:17 -0700

    Added LSS scanning, parsing, and interactive editor.

--- ShoefitterDX/Program.cs
+++ ShoefitterDX/Program.cs
@@ -18,19 +18,31 @@ namespace ShoefitterDX
         [STAThread]
         static void Main()
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
             Config = new SAGESharp.INIConfig(INIFilename);
 
             SAGESharp.OSI.OSIFile osi;
 
-            using (System.IO.FileStream stream = new System.IO.FileStream("D:\\Program Files\\LEGO Bionicle\\Data\\Base.osi", System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.Read))
+            string osiFilename = "D:\\Program Files\\LEGO Bionicle\\Data\\Base.osi";
+
+            if (!System.IO.File.Exists(osiFilename))
+            {
+                OpenFileDialog osiBrowser = new OpenFileDialog();
+                osiBrowser.Filter = "SAGE OSI File (*.osi)|*.osi";
+                if (osiBrowser.ShowDialog() == DialogResult.Cancel)
+                    return;
+                osiFilename = osiBrowser.FileName;
+            }
+
+            using (System.IO.FileStream stream = new System.IO.FileStream(osiFilename, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.Read))
             using (System.IO.BinaryReader reader = new System.IO.BinaryReader(stream))
             {
                 osi = new SAGESharp.OSI.OSIFile(reader);
 
             }
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Window());
 
             foreach (SAGESharp.OSI.OSIFile.FunctionInfo func in osi.Functions)
@@ -62,9 +74,9 @@ namespace ShoefitterDX
                 }
             }
 
-            //OSIBrowser browser = new OSIBrowser();
-            //browser.LoadOSI(osi);
-            //Application.Run(browser);
+            /*OSIBrowser browser = new OSIBrowser();
+            browser.LoadOSI(osi);
+            Application.Run(browser);*/
 
             LSSInteractive IDE = new LSSInteractive();
             Application.Run(IDE);
